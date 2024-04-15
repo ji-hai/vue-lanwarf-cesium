a8caceec53aa51fb8781c2e415c19b5063ada32c
吉海 <2661463169@qq.com>
2024-04-15 20:22:24 +0800

    初始化项目框架

---
+++ mock/role/index.mock.ts
@@ -0,0 +1,94 @@
+import { MockMethod } from 'vite-plugin-mock'
+import { SUCCESS_CODE } from '@/constants'
+
+const timeout = 1000
+
+const adminList = [
+	{
+		path: '/baseMap',
+		component: '#',
+		redirect: '/baseMap/TypeMap/TDT',
+		name: 'baseMap',
+		meta: {
+			title: '基本图层',
+			icon: 'carbon:skill-level-advanced'
+		},
+		children: [
+			{
+				path: 'TypeMap',
+				name: 'TypeMap',
+				component: '##',
+				redirect: '/baseMap/TypeMap/TDT',
+				meta: {
+					title: '图层类型'
+				},
+				children: [
+					{
+						path: 'TDT',
+						name: 'TDT',
+						component: 'views/BaseMap/TDT',
+						meta: {
+							title: '天地图'
+						}
+					},
+					{
+						path: 'TDT1',
+						name: 'TDT1',
+						component: 'views/BaseMap/TDT',
+						meta: {
+							title: 'router.menu12'
+						}
+					}
+				]
+			},
+			{
+				path: 'menu2',
+				name: 'Menu2Demo',
+				component: 'views/BaseMap/TDT',
+				meta: {
+					title: '线效果'
+				}
+			},
+		]
+	},
+	{
+		path: '/line',
+		component: '#',
+		redirect: '/line',
+		name: 'line',
+		meta: {
+			title: '线图层',
+			icon: 'carbon:skill-level-advanced'
+		},
+		children: [
+			{
+				path: 'line1',
+				name: 'line1',
+				component: 'views/BaseMap/TDT',
+				meta: {
+					title: '线效果'
+				}
+			},
+		]
+	},
+]
+
+const testList: string[] = [
+  '/baseMap',
+]
+
+export default [
+  // 列表接口
+  {
+    url: '/mock/role/list',
+    method: 'get',
+    timeout,
+    response: ({ query }) => {
+      const { roleName } = query
+      return {
+        code: SUCCESS_CODE,
+        data: roleName === 'admin' ? adminList : testList
+      }
+    }
+  }
+] as MockMethod[]
---
+++ src/views/Error/404.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Error } from '@/components/Error'
+import { usePermissionStore } from '@/store/modules/permission'
+import { useRouter } from 'vue-router'
+
+const { push } = useRouter()
+
+const permissionStore = usePermissionStore()
+
+const errorClick = () => {
+  push(permissionStore.addRouters[0]?.path as string)
+}
+</script>
+
+<template>
+  <div class="flex justify-center m-t-10">
+    <Error @error-click="errorClick" />
+  </div>
+</template>
